b2c1699bf0b18dcd60c9d7a5ce66969ca3391fac
James Rydberg <jrrydberg23@gmail.com>
2021-06-08 23:11:21 -0500

    Added function to color code time blocks based on past present and future.

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,22 @@
+// Display current date
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$("#currentDay").text(today.format("dddd, MMMM Do YYYY"));
+
 
 console.log(today)
+
+// Color code rows based on past present and future
+var timeBlock = $(".hour");
+var currentTime = parseInt(moment().format("H"));
+
+$each(timeBlock, function (i, hour) {
+    var hourId = parseInt($(this).attr("id"));
+    if (hourId === currentTime) {
+        $(this).next().addClass("present");
+    } else if (hourId < currentTime) {
+        $(this).next().addClass("past");
+    } else if (hourId > currentTime) {
+        $(this).next().addClass("future");
+    }
+});
+
